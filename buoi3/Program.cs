d0cf1d5662907c489cda61219041619911d6850f
dangnguyenbanh <nguyen09727568@gmail.com>
2023-08-30 15:48:44 +0700

    bai3

--- buoi3/Program.cs
+++ buoi3/Program.cs
@@ -11,6 +11,19 @@ namespace buoi3
         static void Main(string[] args)
         {
 
+            TamGiac TG1 = new TamGiac(2, 6, 5);
+            TamGiac TG2 = new TamGiac(3, 3, 3);
+            TamGiac TG3 = new TamGiac(3, 5, 3);
+            TamGiac TG4 = new TamGiac(-1, 6, 5);
+            Console.WriteLine("Tam giac 1 la");
+            TG1.xuat();
+            Console.WriteLine("Tam giac 2 la");
+            TG2.xuat();
+            Console.WriteLine("Tam giac 3 la");
+            TG3.xuat();
+            Console.WriteLine("Tam giac 4 la");
+            TG4.xuat();
+            Console.ReadKey();
         }
     }
 }
--- buoi3/TamGiac.cs
+++ buoi3/TamGiac.cs
@@ -29,7 +29,57 @@ namespace buoi3
             get { return CanhC; }
             set { CanhC = value; }
         }
-
-
+        public TamGiac()
+        {
+            CanhA = 0;
+            CanhB = 0;
+            CanhC = 0;
+        }
+        public TamGiac(double A,double B,double C)
+        {
+            if (A <= 0 || B <= 0 || C <= 0 || A + B <= C || A + C <= B || B + C <= A)
+            {
+                CanhA = 0;
+                CanhB = 0;
+                CanhC = 0;
+            }
+            else
+            CanhA = A;
+            CanhB = B;
+            CanhC = C;
+        }
+        public double TinhChuVi()
+        {
+            return CanhA + CanhB + CanhC;
+        }
+        public double TinhDienTich()
+        {
+            double p = TinhChuVi() / 2;
+            return Math.Sqrt(p * (p - CanhA) * (p - CanhB) * (p - CanhC));
+        }
+        public string LoaiTamGiac()
+        {
+            if (CanhA == CanhB && CanhB == CanhC)
+            {
+                return "Tam giac deu";
+            }
+            else if( CanhA ==CanhB || CanhB==CanhC || CanhA==CanhC)
+            {
+                return "Tam giac can";
+            }
+            else if (CanhA + CanhB >= CanhC && CanhA + CanhC >= CanhB && CanhB + CanhC >= CanhA)
+            {
+                return "tam giac thuong";
+            }
+            else
+            {
+                return "day khong phai la tam giac";
+            }
+        }
+        public void xuat()
+        {
+            Console.WriteLine("Ba canh cua tam giac la({0} ; {1} ;  {2}) ;tam giac nay la {3} ;  Chu vi tam giac la {4}, Dien tich tam giac la {5}", CanhA, CanhB, CanhC, LoaiTamGiac(), TinhChuVi(), TinhDienTich());
+        }   
     }
+
 }
