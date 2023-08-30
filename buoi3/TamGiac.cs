309e7b6e9fe5c38372e61b4412f64ee54be6ee40
dangnguyenbanh <nguyen09727568@gmail.com>
2023-08-30 14:15:33 +0700

    add class Tamgiac.cs

---
+++ buoi3/TamGiac.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buoi3
+{
+    class TamGiac
+    {
+        private double CanhA;
+
+        public double CanhA1
+        {
+            get { return CanhA; }
+            set { CanhA = value; }
+        }
+        private double CanhB;
+
+        public double CanhB1
+        {
+            get { return CanhB; }
+            set { CanhB = value; }
+        }
+        private double CanhC;
+
+        public double CanhC1
+        {
+            get { return CanhC; }
+            set { CanhC = value; }
+        }
+
+
+    }
+}
